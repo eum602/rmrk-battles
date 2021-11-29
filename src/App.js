5094f3a92a398c8998f71695a848aa620b592ea2
jerson.miranda <jerson.miranda@outlook.es>
2021-11-28 19:28:16 -0500

    feat: add game mechanincs

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React, {useState, useEffect} from "react"
 // import RmrkApi from './api/RmrkApi';
 // import Play from "./components/play";
 // import WalletConnector from './components/polkadotConnector';
-//import RMRK2SocketServerConnector from "./components/rmrk2SocketServerConnector";
 import RMRK2SocketServerConnector from "./components/RMRK2SocketServerConnector";
-
 import Game from "./components/game";
 
 /*"EVPRszrKPmDT5hwGNhnxp2N1LBKrJgTujpay6xmzQDr4RtN"*/
 function App() {
-
-  const [nftId, setNftId] = useState('');
-
-  const onSelectedNftId = (_nftId) => {
-    console.log("Selecting final id in app.js", _nftId)
-    setNftId(_nftId)
-  }
-  
   return (
-    <RMRK2SocketServerConnector nftId={nftId}>
-      <Game onSelectedNftId={onSelectedNftId} />
+    <RMRK2SocketServerConnector>
+      <Game/>
     </RMRK2SocketServerConnector>
   );
 }
